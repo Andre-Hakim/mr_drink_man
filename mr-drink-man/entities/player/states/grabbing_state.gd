6126ea7fa4c63bbda65a1d1480ce564b797f48a4
Andre-Hakim <54047613+Andre-Hakim@users.noreply.github.com>
2026-08-02 16:03:26 -0400

    adding woosh random and hand grab sound

--- mr-drink-man/entities/player/states/grabbing_state.gd
+++ mr-drink-man/entities/player/states/grabbing_state.gd
@@ -1,7 +1,6 @@
 extends State
 
 func enter():
-	on_enter_audio.play()
 	main_animation_player.play("hand_grabbing")
 	await main_animation_player.animation_finished
 	transitioned.emit(self, "grabidle")
