a11cc9439107d2e0aba3bfe9a9bc51d1fd816365
Jacob Millner <jacob.millner@outlook.com>
2026-08-02 15:27:43 -0400

    start windup

--- mr-drink-man/entities/player/states/idle_state.gd
+++ mr-drink-man/entities/player/states/idle_state.gd
@@ -4,5 +4,5 @@ func enter():
 	main_animation_player.play("hand_idle")
 
 func physics_update(_delta: float):
-	if Input.is_action_just_pressed("right_hand"):
+	if Input.is_action_just_pressed("start_windup"):
 		transitioned.emit(self, "windup")
