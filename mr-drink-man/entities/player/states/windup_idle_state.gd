b2f442b6ecfa73be875655e50cb50ef81f278cba
Jacob Millner <jacob.millner@outlook.com>
2026-08-02 14:28:17 -0400

    wind up and wind up idle

--- mr-drink-man/entities/player/states/windup_idle_state.gd
+++ mr-drink-man/entities/player/states/windup_idle_state.gd
@@ -1,1 +1,8 @@
 extends State
+
+func enter():
+	main_animation_player.play("hand_windup_idle")
+
+func physics_update(_delta: float):
+	if Input.is_action_just_pressed("right_hand"):
+		transitioned.emit(self, "grabbed")
